46d58a1ec5b37b66cdfff4d92c085b93a8d0a8e2
admin <bytelabz@gmail.com>
2020-12-05 16:59:53 +0100

    Initial commit

---
+++ src/test/java/at/fz/DemoSpringApp/DemoSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fz.DemoSpringApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
